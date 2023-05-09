29d5ac6ee7f189d7359b89b16f5f88f69a34b3e9
kenydanfer01 <kenychumacero19@gmail.com>
2023-05-09 12:37:24 -0500

    Add Exceptions in driven/jpa/mysql

---
+++ src/main/java/com/pragma/powerup/plazoletamicroservice/adapters/driven/jpa/mysql/exceptions/OwnerAlreadyAssignedException.java
@@ -0,0 +1,7 @@
+package com.pragma.powerup.plazoletamicroservice.adapters.driven.jpa.mysql.exceptions;
+
+public class OwnerAlreadyAssignedException extends RuntimeException{
+    public OwnerAlreadyAssignedException(){
+        super();
+    }
+}
